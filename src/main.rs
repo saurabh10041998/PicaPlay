6a9cb59d9a8613bedf779bd047e03145c69ce9d3
saurabh10041998 <shindesaurabhpict@gmail.com>
2023-04-08 16:46:12 -0400

    Printing TCP stream packets only, will handle rest protocols later

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
 use pcap::Capture;
-use pnet::packet::Packet;
-use pnet::packet::ethernet::{EthernetPacket, EtherTypes};
-use pnet::packet::ipv4::Ipv4Packet;
 use pnet::packet::arp::ArpPacket;
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::tcp::TcpPacket;
+use pnet::packet::Packet;
 
 fn process_ipv4_packet(i_packet: &Ipv4Packet) {
-    println!("{:?} -> {:?}", i_packet.get_source(), i_packet.get_destination());
+    match i_packet.get_next_level_protocol() {
+        IpNextHeaderProtocols::Tcp => {
+            let t_packet = TcpPacket::new(i_packet.payload());
+            match t_packet {
+                Some(tcp_packet) => {
+                    println!(
+                        "{:?}:{:?} -> {:?}:{:?}",
+                        i_packet.get_source(),
+                        tcp_packet.get_source(),
+                        i_packet.get_destination(),
+                        tcp_packet.get_destination()
+                    );
+                }
+                None => {
+                    println!("[!!] Sorry, unable to forge tcp packet");
+                }
+            }
+        }
+        IpNextHeaderProtocols::Udp => {
+            // TODO: work on these protos as well
+            //println!("[+] Udp packet detected !!");
+        }
+        _ => println!("[!!] Out of syllabus"),
+    }
 }
-
 
 fn process_ether_packet(e_packet: &EthernetPacket) {
     match e_packet.get_ethertype() {
@@ -16,28 +40,28 @@ fn process_ether_packet(e_packet: &EthernetPacket) {
             match ip_packet {
                 Some(i_packet) => {
                     process_ipv4_packet(&i_packet);
-                },
+                }
                 None => {
                     println!("[!!] Cannot forge IPv4 packet");
                 }
             }
-        },
-        EtherTypes::Arp =>  {
+        }
+        EtherTypes::Arp => {
             let arp_packet = ArpPacket::new(e_packet.payload());
             match arp_packet {
                 Some(_a_packet) => {
+                    // TODO: work on these protos as well
                     //println!("[+] Able to forge Arp packet");
                     //println!("{:?}", a_packet);
-                }, 
+                }
                 None => {
                     println!("[!!] Cannot forge ARP packet");
                 }
             }
-        },
-        _ => println!("[!!] Out of syllabus")
+        }
+        _ => println!("[!!] Out of syllabus"),
     }
 }
-
 
 fn main() {
     let mut cap = Capture::from_file("test.pcapng").unwrap();
@@ -48,7 +72,7 @@ fn main() {
                 //println!("[+] Able to detect ethernet layer : {:?}", e_packet);
                 // Process ethernet packet...
                 process_ether_packet(&e_packet);
-            },
+            }
             None => {
                 println!("[!!] No ethernet layer present: {:?}", packet)
             }
