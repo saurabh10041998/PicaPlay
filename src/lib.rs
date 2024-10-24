c9749274fd4e3cfce5ef9f717a6c895ccddee987
saurabh10041998 <shindesaurabhpict@gmail.com>
2024-10-24 19:53:29 +0530

    Simple test exploring pcap, count number of packets in pcap

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,29 @@
-pub fn sample_test() -> String { 
+use pcap::Error;
+pub fn sample_test() -> String {
     String::from("This is test string")
+}
+
+pub fn get_packet_count(pcap_name: &'static str) -> Result<usize, Error> {
+    let mut count = 0;
+    let mut capture = pcap::Capture::from_file(pcap_name)?;
+    while let Ok(_) = capture.next_packet() {
+        count += 1;
+    }
+    Ok(count)
 }
 
 #[cfg(test)]
 pub mod tests {
     use super::*;
     #[test]
-    fn test_simple_string_return() { 
+    fn test_simple_string_return() {
         assert_eq!(sample_test(), String::from("This is test string"));
+    }
+
+    #[test]
+    fn test_counting_pcaps() {
+        let count_result = get_packet_count("pcaps/bgp.pcap");
+        assert!(count_result.is_ok());
+        assert_eq!(count_result.unwrap(), 157);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,9 @@ use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::packet::ipv4;
 use pnet::packet::ipv4::{Ipv4Packet, MutableIpv4Packet};
 use pnet::packet::tcp;
-use pnet::packet::tcp::{TcpPacket, MutableTcpPacket};
-use pnet::packet::{Packet, MutablePacket};
+use pnet::packet::tcp::{MutableTcpPacket, TcpPacket};
+use pnet::packet::{MutablePacket, Packet};
 use std::net::Ipv4Addr;
-
 
 fn modify_tcp_fields(t_packet: &TcpPacket, ipv4_source: &Ipv4Addr, ipv4_destination: &Ipv4Addr) {
     println!("[+] original tcp packet: {:#?}", t_packet);
@@ -20,13 +19,12 @@ fn modify_tcp_fields(t_packet: &TcpPacket, ipv4_source: &Ipv4Addr, ipv4_destinat
         n_t_packet.set_source(8080);
         n_t_packet.set_destination(443);
         // Set the checksum
-        let checksum = tcp::ipv4_checksum(&n_t_packet.to_immutable(), ipv4_source, ipv4_destination);
-        n_t_packet.set_checksum(checksum);        
+        let checksum =
+            tcp::ipv4_checksum(&n_t_packet.to_immutable(), ipv4_source, ipv4_destination);
+        n_t_packet.set_checksum(checksum);
         println!("[+] New tcp packet: {:#?}", n_t_packet);
 
-
-
-        // Prepare Ipv4 and Etherpacket   
+        // Prepare Ipv4 and Etherpacket
         let mut ip_buff = vec![0; 20 + n_t_packet.packet().len()];
         println!("Reserving: {:?}", ip_buff.len());
         let mut ipv4_packet = MutableIpv4Packet::new(&mut ip_buff).unwrap();
@@ -34,11 +32,11 @@ fn modify_tcp_fields(t_packet: &TcpPacket, ipv4_source: &Ipv4Addr, ipv4_destinat
         ipv4_packet.set_destination(ipv4_destination.clone());
         let ip_checksum = ipv4::checksum(&ipv4_packet.to_immutable());
         ipv4_packet.set_checksum(ip_checksum);
-        ipv4_packet.set_total_length((20 + n_t_packet.packet().len()) as  u16);
+        ipv4_packet.set_total_length((20 + n_t_packet.packet().len()) as u16);
         ipv4_packet.set_payload(n_t_packet.packet_mut());
 
         println!("[+] Crafted ipv4 packet {:#?}", ipv4_packet);
-    }    
+    }
 }
 
 fn process_ipv4_packet(i_packet: &Ipv4Packet) {
@@ -54,7 +52,11 @@ fn process_ipv4_packet(i_packet: &Ipv4Packet) {
                         i_packet.get_destination(),
                         tcp_packet.get_destination()
                     );
-                    modify_tcp_fields(&tcp_packet, &i_packet.get_source(), &i_packet.get_destination());
+                    modify_tcp_fields(
+                        &tcp_packet,
+                        &i_packet.get_source(),
+                        &i_packet.get_destination(),
+                    );
                 }
                 None => {
                     println!("[!!] Sorry, unable to forge tcp packet");
